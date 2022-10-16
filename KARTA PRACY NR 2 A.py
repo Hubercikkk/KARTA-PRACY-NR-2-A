f6aaf6bd1f2baa1f672fde5a8b46c27794f524d2
1B11HK <114583596+Hubercikkk@users.noreply.github.com>
2022-10-16 16:22:56 +0000

    Initial commit

---
+++ KARTA PRACY NR 2 A.py
@@ -0,0 +1,20 @@
+# zad 1
+ import math
+
+# a, b = int(input()), int(input())
+#
+# if (a+b)%2==0:
+#     print
+
+# zad 2
+# a= int(input())
+# b= int(input())
+#
+#ŚA = (a+b)/2
+#ŚG = (a*b)**(1/2)
+#
+#if ŚA > ŚG:
+#    print"TAK"
+#else:
+#    print"NIE"
+  
